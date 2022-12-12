f74d1fa1142174eae79bd7e235a099575bb42214
nikhil thengane <nikhilthengane700@gmail.com>
2022-12-12 09:33:21 +0530

    Application Car

---
+++ src/com/bootcoding/java/oops/Application.java
@@ -0,0 +1,26 @@
+package com.bootcoding.java.oops;
+
+public class Application
+{
+    public static void main(String[] args) {
+        Car car=new Car();
+        car.startEngine();
+        car.move();
+
+
+        car.carOwner = "Nikhil Thengane";
+        car.brandName="Tata";
+        car.model= "Harrier";
+        car.engine= "TDI";
+        car.fuelType="Petrol";
+        car.colour="Black";
+        car.lights=4;
+        car.seats=5;
+        car.print();
+
+
+
+    }
+
+
+}
---
+++ src/com/bootcoding/java/oops/Car.java
@@ -0,0 +1,33 @@
+package com.bootcoding.java.oops;
+
+public class Car {
+
+        String colour;
+        String model;
+        String brandName;
+        String engine;
+        String carOwner;
+        int lights;
+        int seats;
+        String fuelType;
+
+        public  void startEngine(){
+            System.out.println("Engine Started");
+        }
+        public void move(){
+            System.out.println("Car is moving");
+        }
+        public void print(){
+            System.out.println("Car Owner = " + carOwner);
+            System.out.println("Car Brand Name = " + brandName);
+            System.out.println("Car Model = " + model);
+            System.out.println("Car Engine Type = " + engine);
+            System.out.println("Car Fuel Type = " + fuelType);
+            System.out.println("Car Color = " + colour);
+            System.out.println("Car Lights = " + lights);
+            System.out.println("Car Seats = " + seats);
+
+
+
+        }}
+
