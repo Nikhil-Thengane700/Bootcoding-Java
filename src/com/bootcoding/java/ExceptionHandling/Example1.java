b93671d1dd5b244a459b3cb6e19195e7443b0d27
nikhil thengane <nikhilthengane700@gmail.com>
2023-02-19 14:49:55 +0530

    Exception Handling

---
+++ src/com/bootcoding/java/ExceptionHandling/Example1.java
@@ -0,0 +1,15 @@
+package com.bootcoding.java.ExceptionHandling;
+
+public class Example1 {
+    public static void main(String[] args) {
+
+        System.out.println("I am in main ");
+        show();
+    }
+
+    private static void show() {
+        System.out.println("I am in show ");
+    }
+
+
+}
---
+++ src/com/bootcoding/java/ExceptionHandling/Example2.java
@@ -0,0 +1,12 @@
+package com.bootcoding.java.ExceptionHandling;
+
+public class Example2 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("I am in main");
+        show();
+    }
+
+    private static void show() {
+        System.out.println(" I am in show");
+    }
+}
