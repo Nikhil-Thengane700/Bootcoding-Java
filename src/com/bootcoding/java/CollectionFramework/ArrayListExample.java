5d034da343b34d21dbbbae45cb747a8d49c38f8a
nikhil thengane <nikhilthengane700@gmail.com>
2023-03-27 21:22:37 +0530

    Collection Framework

---
+++ src/com/bootcoding/java/CollectionFramework/ArrayListExample.java
@@ -0,0 +1,33 @@
+package com.bootcoding.java.CollectionFramework;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Raja");
+        list.add("Banti");
+        list.add("Babli");
+        list.add("Babu");
+        list.add("Jai");
+        list.add("Raju");
+
+        //Traditional loop
+
+        for (int i = 0; i < list.size() ; i++) {
+            System.out.println(list.get(i));
+        }
+
+        // for each loop
+     /*   for(String str : list){
+            System.out.println(str);
+        }
+
+      */
+
+        //For each method in java 8
+
+        list.forEach(str-> System.out.println(str));
+    }
+}
