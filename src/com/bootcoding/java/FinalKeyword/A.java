6ebf47e8da9a23f9952a9aae1abb1dce6e744d2f
nikhil thengane <nikhilthengane700@gmail.com>
2023-03-29 22:38:53 +0530

    Final Keyword

---
+++ src/com/bootcoding/java/FinalKeyword/A.java
@@ -0,0 +1,11 @@
+package com.bootcoding.java.FinalKeyword;
+
+final class A {
+    void show(){
+        System.out.println("I am in A");
+    }
+    void showA(){
+        System.out.println("This is show A");
+    }
+
+}
---
+++ src/com/bootcoding/java/FinalKeyword/Demo.java
@@ -0,0 +1,9 @@
+package com.bootcoding.java.FinalKeyword;
+
+public class Demo {
+    public static void main(String[] args) {
+        A a1 = new A();
+        a1.show();
+        a1.showA();
+    }
+}
