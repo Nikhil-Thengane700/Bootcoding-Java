5d6aed3f54ba71f40ce9fe403c41dad41e35534f
nikhil thengane <nikhilthengane700@gmail.com>
2023-03-29 22:35:56 +0530

    OOPS: Inheritance

---
+++ src/com/bootcoding/java/OOPS/Inheritance/Example2/A.java
@@ -0,0 +1,9 @@
+package com.bootcoding.java.OOPS.Inheritance.Example2;
+
+public class A {A(){
+    System.out.println("Default constructor of A");
+}
+    A(int n){
+        System.out.println("Parameterized constructor from A");
+    }
+}
---
+++ src/com/bootcoding/java/OOPS/Inheritance/Example3/A.java
@@ -0,0 +1,7 @@
+package com.bootcoding.java.OOPS.Inheritance.Example3;
+
+public class A {
+    A(int n ){
+        System.out.println("Parameterized Constructor of A");
+    }
+}
