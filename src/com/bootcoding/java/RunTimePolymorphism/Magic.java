4f34645dbefdcf07c5d01d4cb6f43ada4e7c0fe9
nikhil thengane <nikhilthengane700@gmail.com>
2023-04-19 20:45:09 +0530

    Run Time Polymorphism

---
+++ src/com/bootcoding/java/RunTimePolymorphism/Magic.java
@@ -0,0 +1,9 @@
+package com.bootcoding.java.RunTimePolymorphism;
+
+public class Magic {
+    public void showMagic(Sim sim){
+        System.out.println("Start");
+        sim.inserted();
+        System.out.println("End");
+    }
+}
