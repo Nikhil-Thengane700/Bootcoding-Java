b5d2930c3bba234f78b49e63b3609ad9bce24b60
nikhil thengane <nikhilthengane700@gmail.com>
2023-04-19 20:48:04 +0530

    Run Time Polymorphism

---
+++ src/com/bootcoding/java/RunTimePolymorphism/Demo.java
@@ -0,0 +1,13 @@
+package com.bootcoding.java.RunTimePolymorphism;
+
+public class Demo {
+    public static void main(String[] args) {
+        Magic m = new Magic();
+        Idea idea = new Idea();
+        Airtel airtel = new Airtel();
+        Jio jio = new Jio();
+        m.showMagic(idea);
+        m.showMagic(airtel);
+        m.showMagic(jio);
+    }
+}
