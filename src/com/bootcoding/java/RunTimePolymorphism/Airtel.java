b678771764aa02d969927f340662368fb4d3419c
nikhil thengane <nikhilthengane700@gmail.com>
2023-04-19 20:39:04 +0530

    Run Time Polymorphism

---
+++ src/com/bootcoding/java/RunTimePolymorphism/Airtel.java
@@ -0,0 +1,8 @@
+package com.bootcoding.java.RunTimePolymorphism;
+
+public class Airtel extends Sim{
+    @Override
+    void inserted() {
+        System.out.println("Airtel Sim inserted");
+    }
+}
