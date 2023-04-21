eefe9085f73c0e50b85638da6fa90569a814f58b
nikhil thengane <nikhilthengane700@gmail.com>
2023-04-21 22:46:05 +0530

    Encapsulation-Private Constructor

--- src/com/bootcoding/java/OOPS/Encapsulation/PrivateConstructor/Hospital.java
+++ src/com/bootcoding/java/OOPS/Encapsulation/PrivateConstructor/Hospital.java
@@ -1,10 +1,16 @@
-package com.bootcoding.java.OOPS.Encapsulation;
+package com.bootcoding.java.OOPS.Encapsulation.PrivateConstructor;
 
 import java.util.Date;
+import java.util.UUID;
 
 public class Hospital {
+    public String name;
+    private UUID id;
+    String address;
+    String services;
     public static void main(String[] args) {
         Patient p = Patient.get();
+     
 
         p.name="ramesh";
         p.age=23;
