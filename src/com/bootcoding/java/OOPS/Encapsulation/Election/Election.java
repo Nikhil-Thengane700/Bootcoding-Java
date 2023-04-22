fa13821c73f8bde847d2ca192b7acc687c4aa3cd
nikhil thengane <nikhilthengane700@gmail.com>
2023-04-22 19:07:32 +0530

    Encapsulation

---
+++ src/com/bootcoding/java/OOPS/Encapsulation/Election/Election.java
@@ -0,0 +1,15 @@
+package com.bootcoding.java.OOPS.Encapsulation.Election;
+
+public class Election {
+    private int electionDate;
+    private String city;
+    private int noOfCandidate;
+
+    public void print(){
+        System.out.println("Election Date"+ electionDate);
+        System.out.println("City "+ city);
+        System.out.println("No of Candidate "+ noOfCandidate);
+    }
+
+
+}
