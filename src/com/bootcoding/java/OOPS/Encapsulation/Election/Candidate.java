77ed7459d3b32bcdc7330e0d1c5a0afa1aa35028
nikhil thengane <nikhilthengane700@gmail.com>
2023-04-22 19:06:54 +0530

    Encapsulation

---
+++ src/com/bootcoding/java/OOPS/Encapsulation/Election/Candidate.java
@@ -0,0 +1,40 @@
+package com.bootcoding.java.OOPS.Encapsulation.Election;
+
+public class Candidate {
+    private String name;
+    private String partyName;
+    private String income;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPartyName() {
+        return partyName;
+    }
+
+    public void setPartyName(String partyName) {
+        this.partyName = partyName;
+    }
+
+    public String getIncome() {
+        return income;
+    }
+
+    public void setIncome(String income) {
+        this.income = income;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
